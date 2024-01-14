e439978cada4b9835dce65bc4eb6879e9251f486
haitham783 <151958911+haitham783@users.noreply.github.com>
2024-01-14 04:28:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import asyncio
+
+from highrise import *
+from highrise import BaseBot, Position, __main__
+from highrise.models import *
+from highrise.models import (
+    Position,
+    SessionMetadata,
+)
+
+from webserver import keep_alive
+
+name_ad = ["haitham783"]
+class BotDefinition:
+    def __init__(self, bot, room_id, api_token):
+      self.bot = bot
+      self.room_id = room_id
+      self.api_token = api_token
+      self.following_username = None
+
+
+class MyBot(BaseBot):
+
+    def __init__(self, bot, room_id, api_token):
+      self.bot = bot
+      self.room_id = room_id
+      self.api_token = api_token
+      self.following_username = None
+      super().__init__()
+      self.user_positions = {}
+      self.is_dancing = False
+    
+    
+
+
+
+    async def on_start(self, session_metadata: SessionMetadata) -> None:
+      print("hi im alive?")
+      try:
+        await self.highrise.walk_to( Position(x=15.5, y=0.5, z=22.0, facing='FrontRight'))
+      except Exception as e:
+        print(f"An exception occurred: {e}")
+
+
+
+    
+
+
+
+
+
+    
+
+      # If no matching function is found
+      
+
+    
+    
+
+    async def run(self):
+       definitions = [BotDefinition(self, self.room_id, self.api_token)]
+       await __main__.main(definitions)
+
+keep_alive()
+if __name__ == "__main__":
+    room_id = "65580096bd12e53ae7c3a64d"
+    token = "e90a475c8ffd5d3df0ffb6f3cbbe50f8307e092768a3bcdd6e60d81a167373ff"
+    bot = Highrise() 
+    bot_instance = MyBot(bot, room_id, token)
+    asyncio.run(bot_instance.run())
